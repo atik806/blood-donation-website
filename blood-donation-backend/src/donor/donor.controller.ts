60e2b3b904571416c48d1efa8de4290d6e17e460
atik806 <atikrj8@gmail.com>
2026-05-08 03:08:52 +0600

    added some controller functionality

--- blood-donation-backend/src/donor/donor.controller.ts
+++ blood-donation-backend/src/donor/donor.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Get, Param, Patch, Delete } from '@nestjs/common';
 import { DonorService } from './donor.service';
 import { Body } from '@nestjs/common';
 import { CreateDonorDto } from './Create.donor.dto';
@@ -17,7 +17,28 @@ export class DonorController {
     )
     {
         return this.donorService.createDonor(createDonorDto);
-
     }
 
+    @Get(':id')
+    getDonorById(@Param('id') id: string) {
+        return this.donorService.getDonorById(+id);
+    }
+
+    @Get()
+    getAllDonors() {
+        return this.donorService.getAllDonors();
+    }
+
+    @Patch(':id')
+    updateDonor(
+        @Param('id') id: string,
+        @Body() updateData: any,
+    ) {
+        return this.donorService.updateDonor(+id, updateData);
+    }
+
+    @Delete(':id')  
+    deleteDonor(@Param('id') id: string) {
+        return this.donorService.deleteDonor(+id);
+    }
 }
