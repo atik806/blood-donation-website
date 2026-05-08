1d20549845940afe2e6d26d5a0ce0503e069ef2c
atik806 <atikrj8@gmail.com>
2026-05-08 10:13:33 +0600

    adding auth service, loginDto, jwtGuard, jwt strategy etc..

--- blood-donation-backend/src/auth/auth.service.ts
+++ blood-donation-backend/src/auth/auth.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { DonorService } from '../donor/donor.service';
+import { CreateDonorDto } from 'src/donor/Create.donor.dto';
+
+
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
-export class AuthService {
+
+    export class AuthService {
+        constructor(
+            private readonly donorService:DonorService,
+        ){}
+
+    public async registerDonor(createDonorDto: CreateDonorDto){
+        const pass = createDonorDto.password;
+        const hashedPassword = await bcrypt.hash(pass, 10);
+        createDonorDto.password = hashedPassword;
+        return await this.donorService.createDonor(createDonorDto);
+    }
     
+
 }
+
+
