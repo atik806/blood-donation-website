bc48820dc1d4d2881c78bae55a08ca3f20d61b27
atik806 <atikrj8@gmail.com>
2026-05-12 19:04:12 +0600

    bug fix

--- blood-donation-frontend/src/app/Login/page.js
+++ blood-donation-frontend/src/app/Login/page.js
@@ -18,11 +18,11 @@ export default function Login() {
           
           <div>
             <label className="block mb-1 font-medium text-black">
-              Username
+              Email
             </label>
             <input
               type="text"
-              placeholder="Username"
+              placeholder="Email"
               className="w-full px-4 py-2 border border-gray-400 rounded-lg text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-red-400"
             />
           </div>
