62ba161bfb3ee1155f06987e02940376ec37874d
atik806 <atikrj8@gmail.com>
2026-05-13 11:50:30 +0600

    added NavWrapper file

---
+++ blood-donation-frontend/src/app/components/NavWrapper.js
@@ -0,0 +1,18 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import CustomNav from "./customNav";
+
+export default function NavWrapper({ children }) {
+  const pathname = usePathname();
+  const showNav = !pathname.startsWith("/donorDashboard") && 
+                  !pathname.startsWith("/patientDashboard") && 
+                  !pathname.startsWith("/AdminDashboard");
+
+  return (
+    <>
+      {showNav && <CustomNav />}
+      {children}
+    </>
+  );
+}
